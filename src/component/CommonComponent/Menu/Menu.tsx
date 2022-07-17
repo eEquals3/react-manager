43f6861a0f3f9dec2b63dbb807ce9cdc05cb8950
eEquals3 <roman.a.ryazantsev@gmail.com>
2022-07-17 15:29:46 +0300

    Fix: меню

--- src/component/CommonComponent/Menu/Menu.tsx
+++ src/component/CommonComponent/Menu/Menu.tsx
@@ -16,9 +16,9 @@ function Menu() {
         arrow={false}
       >
         <div className="menu-subMenu">
-          <div className="menu-item"> команда 1</div>
-          <div className="menu-item"> команда 2</div>
-          <div className="menu-item"> команда 3</div>
+          <button type="button" className="menu-item"> команда 1</button>
+          <button type="button" className="menu-item"> команда 2</button>
+          <button type="button" className="menu-item"> команда 3</button>
         </div>
       </Popup>
       <Popup
