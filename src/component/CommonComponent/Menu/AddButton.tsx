9f303b64d17df645c7cf36f9eba556338ae4afa3
eEquals3 <roman.a.ryazantsev@gmail.com>
2022-07-19 17:55:07 +0300

    нормальные менюшки добавления

--- src/component/CommonComponent/Menu/AddButton.tsx
+++ src/component/CommonComponent/Menu/AddButton.tsx
@@ -8,10 +8,24 @@ interface Prop{
 export default function AddButton({ name }:Prop) {
   const [state, setState] = useState<string>('');
 
+  const editName = useCallback((nameToEdit:string) => {
+    switch (nameToEdit) {
+      case 'Список команд':
+        return 'команда';
+      case 'Статистика':
+        return 'команда';
+      case 'Список задач':
+        return 'задача';
+      default:
+        return '';
+    }
+  }, []);
+
   const clickAddButton = useCallback(() => (
-    console.log(state.length > 3 ? 'ok' : 'nope')
+    console.log(state.length > 3 ? `ok, new ${editName(name)} name ${state}` : 'nope')
   ), [state]);
 
+  /* {`Введите имя ${name}`} */
   const Text = useMemo(() => (
     <label htmlFor="Name" style={{ textAlign: 'center', display: 'block' }}>
       {}
@@ -19,7 +33,7 @@ export default function AddButton({ name }:Prop) {
         type="text"
         onChange={(loginChange) => { setState(loginChange.target.value); }}
         value={state}
-        placeholder={`Введите имя ${name}`}
+        placeholder={editName(name)}
         style={{ borderRadius: '10px', textAlign: 'center' }}
       />
     </label>
@@ -34,10 +48,9 @@ export default function AddButton({ name }:Prop) {
       добавить
     </button>
   ), [clickAddButton]);
-
   return (
     <Modal
-      modalName={`Новая ${name}`}
+      modalName={`Новая ${editName(name)}`}
       triggerButtonName="+"
       triggerButtonStyle="menu-subMenuButtons"
       renderContent={Text}
